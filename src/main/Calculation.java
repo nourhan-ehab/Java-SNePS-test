b71f7e1da30d7036224217fec2a01ade28268ffe
nourhan-ehab <nourhan.ehab@guc.edu.eg>
2018-01-15 11:00:29 +0200

    Update Calculation.java

--- src/main/Calculation.java
+++ src/main/Calculation.java
@@ -1,15 +1,19 @@
 package main;
 public class Calculation 
 {
-public static int add(int a, int b) {
-return a + b;
-}
-public static int sub(int a, int b) {
-return a - b;
+	public Calculation()
+	{
 
-}
-public static int multiply(int a, int b) {
-	return a * b;
-}
+	}
+	public static int add(int a, int b) {
+	return a + b;
+	}
+	public static int sub(int a, int b) {
+	return a - b;
+
+	}
+	public static int multiply(int a, int b) {
+		return a * b;
+	}
 	
 }
