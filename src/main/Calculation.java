c52a3f03cb709420cd5687126324114abb027a2c
nourhan-ehab <nourhan.ehab@guc.edu.eg>
2018-01-20 19:47:29 +0200

    Update Calculation.java

--- src/main/Calculation.java
+++ src/main/Calculation.java
@@ -15,8 +15,7 @@ public class Calculation
 	public static int multiply(int a, int b) {
 		return a * b;
 	}
-	public static int multiply(int a, int b) {
-		return a * b;
+	public static int divide(int a, int b) {
+		return a / b;
 	}
-	
 }
